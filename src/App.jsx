f52a7438e39db84ec1c81ad257f813ec81bff410
Osw0 <5196ksy@gmail.com>
2024-05-01 18:40:48 +0900

    add CreateImage.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,26 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import './App.css';
-import LoginForm from './LoginForm';
-import SignupForm from './SignupForm';
-import MyPage from './MyPage';
-import Modal from './Modal';
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import "./App.css";
+import LoginForm from "./LoginForm";
+import SignupForm from "./SignupForm";
+import MyPage from "./MyPage";
+import Modal from "./Modal";
+import CreateImage from "./CreateImage";
 
 function App() {
-    return (
-        <Router>
-            <div className="App">
-                <Routes>
-                    <Route path="/" element={<LoginForm />} />
-                    <Route path="/SignupForm" element={<SignupForm />} />
-                    <Route path="/MyPage" element={<MyPage />} />
-                    <Route path="/Modal" element={<Modal />} />
-                </Routes>
-            </div>
-        </Router>
-    );
+  return (
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<LoginForm />} />
+          <Route path="/SignupForm" element={<SignupForm />} />
+          <Route path="/MyPage" element={<MyPage />} />
+          <Route path="/Modal" element={<Modal />} />
+          <Route path="/CreateImage" element={<CreateImage />} />
+        </Routes>
+      </div>
+    </Router>
+  );
 }
 
 export default App;
---
+++ src/CreateImage.jsx
@@ -0,0 +1,11 @@
+import React, { useState, useRef } from "react";
+
+const CreateImage = () => {
+  return (
+    <div>
+      <h1>프롬프트로 이미지 생성하는 페이지</h1>
+    </div>
+  );
+};
+
+export default CreateImage;
