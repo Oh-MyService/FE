149289456f91730e7cb76a67e61845beaa3b13a1
zw0n2 <daniela0412@naver.com>
2024-06-18 15:22:30 +0900

    데모영상

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,65 +1,65 @@
-import React from "react";
-import { useNavigate } from "react-router-dom";
-import BG_pattern from "../assets/home_empty_ver.png";
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import BG_pattern from '../assets/home_empty_ver.png';
 
 const images1 = [
-  "slider1.jpg",
-  "slider2.png",
-  "slider3.png",
-  "slider10.jpg",
-  "slider1.jpg",
-  "slider2.png",
-  "slider3.png",
-  "slider10.jpg",
+    'slider1.jpg',
+    'slider2.png',
+    'slider3.png',
+    'slider10.jpg',
+    'slider1.jpg',
+    'slider2.png',
+    'slider3.png',
+    'slider10.jpg',
 ];
 
 const images2 = [
-  "slider5.webp",
-  "slider6.webp",
-  "slider11.webp",
-  "slider8.jpg",
-  "slider5.webp",
-  "slider6.webp",
-  "slider11.webp",
-  "slider8.jpg",
+    'slider5.webp',
+    'slider6.webp',
+    'slider11.webp',
+    'slider8.jpg',
+    'slider5.webp',
+    'slider6.webp',
+    'slider11.webp',
+    'slider8.jpg',
 ];
 
 const slideStyles = {
-  width: "220px",
-  height: "220px",
-  margin: "10px 0",
-  padding: "10px",
-  objectFit: "cover",
-  borderRadius: "20px",
+    width: '220px',
+    height: '220px',
+    margin: '10px 0',
+    padding: '10px',
+    objectFit: 'cover',
+    borderRadius: '20px',
 };
 
 const containerStyles = {
-  position: "absolute",
-  top: "50%",
-  left: "70%",
-  transform: "translate(-50%, -50%)",
-  width: "800px",
-  height: "900px",
-  overflow: "hidden",
+    position: 'absolute',
+    top: '50%',
+    left: '70%',
+    transform: 'translate(-50%, -50%)',
+    width: '800px',
+    height: '900px',
+    overflow: 'hidden',
 };
 
 const sliderStyles = {
-  position: "absolute",
-  display: "flex",
-  flexDirection: "column",
-  flexWrap: "nowrap",
+    position: 'absolute',
+    display: 'flex',
+    flexDirection: 'column',
+    flexWrap: 'nowrap',
 };
 
 const slider1Styles = {
-  ...sliderStyles,
-  right: "260px",
-  animation: "slidedown 15s linear infinite",
+    ...sliderStyles,
+    right: '260px',
+    animation: 'slidedown 15s linear infinite',
 };
 
 const slider2Styles = {
-  ...sliderStyles,
-  right: "-10px",
-  animation: "slideup 15s linear infinite",
+    ...sliderStyles,
+    right: '-10px',
+    animation: 'slideup 15s linear infinite',
 };
 
 const keyframes = `
@@ -75,61 +75,59 @@ const keyframes = `
 `;
 
 function Main() {
-  const navigate = useNavigate();
+    const navigate = useNavigate();
 
-  return (
-    <div
-      className="bg-[#F2F2F2] h-screen w-full bg-cover bg-center bg-no-repeat flex flex-col justify-center items-start p-8 relative overflow-hidden"
-      style={{ backgroundImage: `url(${BG_pattern})` }}
-    >
-      <style>{keyframes}</style>
-      <h1 className="text-6xl font-['pretendard-black'] text-left pl-20">
-        세상의 모든 원단 디자인, 디자이노블에서
-      </h1>
-      <p className="text-2xl text-left font-['pretendard-medium'] pl-20 pt-10">
-        AI와 함께 원하는 디자인 손쉽게 완성!
-      </p>
-      <p className="text-2xl text-left font-['pretendard-medium'] pl-20">
-        지금 바로 만들어보세요
-      </p>
-      <div className="flex space-x-4 mt-4 pl-20 pt-10">
-        <button
-          className="text-2xl bg-[#5E71A6] hover:bg-blue-900 text-white font-['pretendard-semibold'] py-5 px-16 rounded-xl mr-4"
-          onClick={() => navigate("/CreateImage")}
+    return (
+        <div
+            className="bg-[#F2F2F2] h-screen w-full bg-cover bg-center bg-no-repeat flex flex-col justify-center items-start p-8 relative overflow-hidden"
+            style={{ backgroundImage: `url(${BG_pattern})` }}
         >
-          지금 만들어보기
-        </button>
-        <button
-          className="text-2xl bg-[#809DEC] hover:bg-[#5E71A6] text-white font-['pretendard-semibold'] py-5 px-24 rounded-xl"
-          onClick={() => navigate("/Login")}
-        >
-          내 디자인
-        </button>
-      </div>
-      <div style={containerStyles}>
-        <div style={slider1Styles}>
-          {images1.map((src, index) => (
-            <img
-              key={index}
-              src={require(`../assets/${src}`)}
-              alt={`Slide ${index + 1}`}
-              style={slideStyles}
-            />
-          ))}
+            <style>{keyframes}</style>
+            <h1 className="text-6xl font-['pretendard-black'] text-left pl-20">
+                세상의 모든 원단 디자인, 디자이노블에서
+            </h1>
+            <p className="text-2xl text-left font-['pretendard-medium'] pl-20 pt-10">
+                AI와 함께 원하는 디자인 손쉽게 완성!
+            </p>
+            <p className="text-2xl text-left font-['pretendard-medium'] pl-20">지금 바로 만들어보세요</p>
+            <div className="flex space-x-4 mt-4 pl-20 pt-10">
+                <button
+                    className="text-2xl bg-[#3A57A7] hover:bg-blue-900 text-white font-['pretendard-semibold'] py-5 px-16 rounded-xl mr-4"
+                    onClick={() => navigate('/CreateImage')}
+                >
+                    지금 만들어보기
+                </button>
+                <button
+                    className="text-2xl bg-[#8194EC] hover:bg-[#5E71A6] text-white font-['pretendard-semibold'] py-5 px-24 rounded-xl"
+                    onClick={() => navigate('/Login')}
+                >
+                    내 디자인
+                </button>
+            </div>
+            <div style={containerStyles}>
+                <div style={slider1Styles}>
+                    {images1.map((src, index) => (
+                        <img
+                            key={index}
+                            src={require(`../assets/${src}`)}
+                            alt={`Slide ${index + 1}`}
+                            style={slideStyles}
+                        />
+                    ))}
+                </div>
+                <div style={slider2Styles}>
+                    {images2.map((src, index) => (
+                        <img
+                            key={index}
+                            src={require(`../assets/${src}`)}
+                            alt={`Slide ${index + 5}`}
+                            style={slideStyles}
+                        />
+                    ))}
+                </div>
+            </div>
         </div>
-        <div style={slider2Styles}>
-          {images2.map((src, index) => (
-            <img
-              key={index}
-              src={require(`../assets/${src}`)}
-              alt={`Slide ${index + 5}`}
-              style={slideStyles}
-            />
-          ))}
-        </div>
-      </div>
-    </div>
-  );
+    );
 }
 
 export default Main;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
-  theme: {
-    extend: {
-      fontFamily: {
-        "Pretendard-Thin": ["Pretendard-Thin"],
-        "Pretendard-ExtraLight": ["Pretendard-ExtraLight"],
-        "Pretendard-Light": ["Pretendard-Light"],
-        "Pretendard-Regular": ["Pretendard-Regular"],
-        "Pretendard-Medium": ["Pretendard-Medium"],
-        "Pretendard-SemiBold": ["Pretendard-SemiBold"],
-        "Pretendard-Bold": ["Pretendard-Bold"],
-        "Pretendard-ExtraBold": ["Pretendard-ExtraBold"],
-        "Pretendard-Black": ["Pretendard-Black"],
-      },
+    content: ['./src/**/*.{js,jsx,ts,tsx}'],
+    theme: {
+        extend: {
+            borderWidth: {
+                3: '3px',
+            },
+            fontFamily: {
+                'Pretendard-Thin': ['Pretendard-Thin'],
+                'Pretendard-ExtraLight': ['Pretendard-ExtraLight'],
+                'Pretendard-Light': ['Pretendard-Light'],
+                'Pretendard-Regular': ['Pretendard-Regular'],
+                'Pretendard-Medium': ['Pretendard-Medium'],
+                'Pretendard-SemiBold': ['Pretendard-SemiBold'],
+                'Pretendard-Bold': ['Pretendard-Bold'],
+                'Pretendard-ExtraBold': ['Pretendard-ExtraBold'],
+                'Pretendard-Black': ['Pretendard-Black'],
+            },
+        },
     },
-  },
-  plugins: [],
+    plugins: [],
 };
