efe7bf119c7a175d41f0f4abfcfb27c86808b597
zw0n2 <daniela0412@naver.com>
2024-07-06 22:35:10 +0900

    옵션추가

--- src/pages/CreateImage.jsx
+++ src/pages/CreateImage.jsx
@@ -1,10 +1,10 @@
 import React, { useState } from 'react';
-import CollectionAddModal from "../components/CollectionAddModal";
+import CollectionAddModal from '../components/CollectionAddModal';
 import { ReactComponent as DLlogo } from '../assets/designovel_icon_black.svg';
 
 const Bubble = ({ text }) => {
     return (
-        <div className="relative bg-[#444655] text-white pt-2 px-5 rounded-lg h-10 font-['pretendard-medium'] mt-3 before:content-[''] before:absolute before:bg-[#444655] before:w-4 before:h-4 before:rotate-45 before:top-1/2 before:-left-1.5 before:-translate-y-1/2">
+        <div className="relative bg-[#444655] text-white pt-2 px-5 rounded-lg h-auto inline-block font-['pretendard-medium'] mt-3 ml-2 before:content-[''] before:absolute before:bg-[#444655] before:w-4 before:h-4 before:rotate-45 before:top-1/2 before:-left-2 before:-translate-y-1/2">
             "{text}" 생성 결과
         </div>
     );
@@ -14,7 +14,8 @@ const CreateImage = () => {
     const [inputText, setInputText] = useState('');
     const [imageURL, setImageURL] = useState('');
     const [alertMessage, setAlertMessage] = useState('');
-    const [showOptions, setShowOptions] = useState(false);
+    const [showOptions, setShowOptions] = useState(true);
+    const [showAdvancedOptions, setShowAdvancedOptions] = useState(false);
     const [promptText, setPromptText] = useState('');
     const [showResult, setShowResult] = useState(false);
     const [isAddModalOpen, setAddModalOpen] = useState(false);
@@ -63,6 +64,7 @@ const CreateImage = () => {
             console.error('Error occurred:', error);
         }
     };
+
     const handleAddMore = (index) => {
         const updatedResults = results.map((result, idx) => {
             if (idx === index) {
@@ -79,153 +81,169 @@ const CreateImage = () => {
         }
     };
 
-    const toggleOptions = () => {
-        setShowOptions(!showOptions);
-    };
-
-    // 이미지 다운로드
     const handleSaveImage = () => {};
 
-    return (
-      <div className="flex min-h-screen bg-[#F2F2F2] pt-10 pb-10 w-full">
-        <div className="flex flex-col pt-16 w-full">
-          <div className="flex flex-row justify-center">
-            <div className="w-[85%]">
-              <span className="block text-2xl font-['pretendard-extrabold'] text-left text-slate-700 mb-5">
-                상상 속 패턴을 지금 만들어보세요!
-              </span>
-              <textarea
-                type="text"
-                value={inputText}
-                onChange={(e) => setInputText(e.target.value)}
-                onKeyDown={handleKeyDown}
-                className="appearance-none block w-full h-40 bg-gray-200 text-gray-700 rounded py-4 px-4 leading-tight focus:outline-none border-3 border-gray-300"
-                placeholder="ex) Natural wave pattern, background color is blue and waves light yellow"
-              />
-              <div className="flex justify-end">
-                <button
-                  type="submit"
-                  className="mt-2 sm:mt-4 bg-[#3A57A7] hover:bg-blue-900 text-white font-['pretendard-bold'] py-4 px-6 rounded-full"
-                  onClick={handleSubmit}
-                >
-                  Generate
-                </button>
-              </div>
-            </div>
-            <div className="flex flex-row justify-center items-start relative">
-              <button onClick={toggleOptions}>
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  strokeWidth="1.5"
-                  stroke="currentColor"
-                  className="w-6 h-6"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M10.5 6h9.75M10.5 6a1.5 1.5 0 1 1-3 0m3 0a1.5 1.5 0 1 0-3 0M3.75 6H7.5m3 12h9.75m-9.75 0a1.5 1.5 0 0 1-3 0m3 0a1.5 1.5 0 0 0-3 0m-3.75 0H7.5m9-6h3.75m-3.75 0a1.5 1.5 0 0 1-3 0m3 0a1.5 1.5 0 0 0-3 0m-9.75 0h9.75"
-                  />
-                </svg>
-              </button>
-              {showOptions && (
-                <div className="absolute top-10 right-7 w-30 bg-white p-3 rounded-lg shadow-xl">
-                  <span className="block text-lg font-['pretendard-extrabold'] text-left text-slate-700">
-                    Option
-                  </span>
-                  <div className="w-40 h-40 bg-gray-200"></div>
-                </div>
-              )}
-            </div>
-          </div>
+    const toggleAdvancedOptions = () => {
+        setShowAdvancedOptions(!showAdvancedOptions);
+    };
 
-          {results.map((result, index) => (
-            <div
-              key={index}
-              className="flex flex-col justify-center mt-4 bg-gray-200 w-[85%] mx-auto p-6 rounded-lg shadow-lg"
-            >
-              <div className="flex">
-                <DLlogo width="60" height="60" className="mr-6" />
-                <Bubble text={result.promptText} />
-              </div>
-              <div className="grid grid-cols-4 gap-6 mt-8">
-                {[...Array(4)].map((_, i) => (
-                  <div
-                    key={i}
-                    className="flex flex-col justify-between items-center w-60 ml-10"
-                  >
-                    {imageURL && (
-                      <div className="overflow-hidden w-60 h-60 cursor-pointer">
-                        <img
-                          src={imageURL}
-                          alt="Generated Image"
-                          className="w-full h-full object-cover"
-                        />
-                      </div>
-                    )}
-                    <div className="flex justify-between items-center w-full mt-2 font-['pretendard-medium'] text-gray-600">
-                      <p className="text-left">{result.date}</p>
-                      <div className="flex items-center space-x-2">
-                        <button onClick={openAddModal}>
-                          <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            fill="none"
-                            viewBox="0 0 24 24"
-                            stroke-width="2"
-                            stroke="currentColor"
-                            class="w-6 h-6"
-                          >
-                            <path
-                              stroke-linecap="round"
-                              stroke-linejoin="round"
-                              d="M17.593 3.322c1.1.128 1.907 1.077 1.907 2.185V21L12 17.25 4.5 21V5.507c0-1.108.806-2.057 1.907-2.185a48.507 48.507 0 0 1 11.186 0Z"
+    return (
+        <div className="flex min-h-screen bg-[#F2F2F2] pt-20 pb-10 w-full">
+            <div className="flex w-full justify-center px-4">
+                <div className="flex flex-col w-[40%] mx-2">
+                    {/* 입력창 섹션 */}
+                    <div className="flex flex-col justify-start items-start">
+                        <span className="block text-2xl font-['pretendard-extrabold'] text-slate-700 mb-5">
+                            상상 속 패턴을 지금 만들어보세요!
+                        </span>
+                        <div className="relative w-full">
+                            <textarea
+                                type="text"
+                                value={inputText}
+                                onChange={(e) => setInputText(e.target.value)}
+                                onKeyDown={handleKeyDown}
+                                className="appearance-none block w-full h-40 bg-gray-200 text-gray-700 rounded-lg py-4 px-4 leading-tight focus:outline-none border-3 border-[#3A57A7] mb-0"
+                                placeholder="ex) Natural wave pattern, background color is blue and waves light yellow"
                             />
-                          </svg>
-                        </button>
-                        <button onClick={handleSaveImage}>
-                          <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            fill="none"
-                            viewBox="0 0 24 24"
-                            strokeWidth="2"
-                            stroke="currentColor"
-                            className="w-6 h-6"
-                          >
-                            <path
-                              strokeLinecap="round"
-                              strokeLinejoin="round"
-                              d="M3 16.5v2.25A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75V16.5M16.5 12 12 16.5m0 0L7.5 12m4.5 4.5V3"
-                            />
-                          </svg>
-                        </button>
-                      </div>
+                            <div className="absolute bottom-0 right-0 flex w-full justify-between bg-gray-200 rounded-b-lg rounded-tl-lg rounded-tr-lg">
+                                <button
+                                    type="submit"
+                                    className="w-full bg-[#3A57A7] hover:bg-blue-900 text-white font-['pretendard-bold'] py-4 rounded-br-lg rounded-bl-lg"
+                                    onClick={handleSubmit}
+                                >
+                                    생성하기
+                                </button>
+                            </div>
+                        </div>
                     </div>
-                  </div>
-                ))}
-              </div>
-              <div>
-                {result.showButton && (
-                  <button
-                    type="button"
-                    className="mt-4 bg-[#8194EC] hover:bg-[#B8C8FF] text-white font-['pretendard-medium'] py-2 px-12 rounded-full transition duration-200"
-                    onClick={() => handleAddMore(index)}
-                  >
-                    같은 명령어로 더 생성하기
-                  </button>
-                )}
-              </div>
-              {isAddModalOpen && <CollectionAddModal onClose={closeAddModal} />}
+                    {/* 옵션 섹션 */}
+                    {showOptions && (
+                        <div
+                            className={`w-full mt-4 p-3 rounded-lg border-3 border-[#3A57A7] transition-all duration-300 ${
+                                showAdvancedOptions ? 'h-80' : 'h-40'
+                            }`}
+                        >
+                            <span className="block text-lg font-['pretendard-extrabold'] text-left text-slate-700">
+                                고급 설정
+                            </span>
+                            <div className="w-full h-full bg-transparent"></div>
+                            <button
+                                className="w-full mt-2 flex justify-center items-center bg-transparent text-[#3A57A7] hover:text-blue-900 py-2 rounded-lg"
+                                onClick={toggleAdvancedOptions}
+                            >
+                                {showAdvancedOptions ? (
+                                    <svg
+                                        xmlns="http://www.w3.org/2000/svg"
+                                        fill="none"
+                                        viewBox="0 0 24 24"
+                                        strokeWidth="1.5"
+                                        stroke="currentColor"
+                                        className="w-8 h-8"
+                                    >
+                                        <path
+                                            strokeLinecap="round"
+                                            strokeLinejoin="round"
+                                            d="m9.75 9.75 4.5 4.5m0-4.5-4.5 4.5M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
+                                        />
+                                    </svg>
+                                ) : (
+                                    <svg
+                                        xmlns="http://www.w3.org/2000/svg"
+                                        fill="none"
+                                        viewBox="0 0 24 24"
+                                        strokeWidth="1.5"
+                                        stroke="currentColor"
+                                        className="w-8 h-8"
+                                    >
+                                        <path
+                                            strokeLinecap="round"
+                                            strokeLinejoin="round"
+                                            d="M12 9v6m3-3H9m12 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
+                                        />
+                                    </svg>
+                                )}
+                            </button>
+                        </div>
+                    )}
+                </div>
+
+                <div className="flex flex-col w-[60%] mx-2 mt-14 h-[80vh] overflow-y-auto bg-gray-200 p-6 rounded-lg shadow-lg">
+                    {/* 생성 결과 섹션 */}
+                    {results.map((result, index) => (
+                        <div key={index} className="flex flex-col justify-center mt-2 w-full">
+                            <div className="flex">
+                                <DLlogo width="60" height="60" className="mr-6" />
+                                <Bubble text={result.promptText} />
+                            </div>
+                            <div className="grid grid-cols-2 gap-4 mt-8">
+                                {[...Array(4)].map((_, i) => (
+                                    <div key={i} className="flex flex-col justify-between items-center w-40">
+                                        {imageURL && (
+                                            <div className="overflow-hidden w-40 h-40 cursor-pointer">
+                                                <img
+                                                    src={imageURL}
+                                                    alt="Generated Image"
+                                                    className="w-full h-full object-cover rounded-lg"
+                                                />
+                                            </div>
+                                        )}
+                                        <div className="flex justify-between items-center w-full mt-2 font-['pretendard-medium'] text-gray-600">
+                                            <p className="text-left">{result.date}</p>
+                                            <div className="flex items-center space-x-2">
+                                                <button onClick={openAddModal}>
+                                                    <svg
+                                                        xmlns="http://www.w3.org/2000/svg"
+                                                        fill="none"
+                                                        viewBox="0 0 24 24"
+                                                        strokeWidth="2"
+                                                        stroke="currentColor"
+                                                        className="w-6 h-6"
+                                                    >
+                                                        <path
+                                                            strokeLinecap="round"
+                                                            strokeLinejoin="round"
+                                                            d="M17.593 3.322c1.1.128 1.907 1.077 1.907 2.185V21L12 17.25 4.5 21V5.507c0-1.108.806-2.057 1.907-2.185a48.507 48.507 0 0 1 11.186 0Z"
+                                                        />
+                                                    </svg>
+                                                </button>
+                                                <button onClick={handleSaveImage}>
+                                                    <svg
+                                                        xmlns="http://www.w3.org/2000/svg"
+                                                        fill="none"
+                                                        viewBox="0 0 24 24"
+                                                        strokeWidth="2"
+                                                        stroke="currentColor"
+                                                        className="w-6 h-6"
+                                                    >
+                                                        <path
+                                                            strokeLinecap="round"
+                                                            strokeLinejoin="round"
+                                                            d="M3 16.5v2.25A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75V16.5M16.5 12 12 16.5m0 0L7.5 12m4.5 4.5V3"
+                                                        />
+                                                    </svg>
+                                                </button>
+                                            </div>
+                                        </div>
+                                    </div>
+                                ))}
+                            </div>
+                            <div>
+                                {result.showButton && (
+                                    <button
+                                        type="button"
+                                        className="mt-4 bg-[#8194EC] hover:bg-[#B8C8FF] text-white font-['pretendard-medium'] py-2 px-12 rounded-full transition duration-200"
+                                        onClick={() => handleAddMore(index)}
+                                    >
+                                        같은 명령어로 더 생성하기
+                                    </button>
+                                )}
+                            </div>
+                            {isAddModalOpen && <CollectionAddModal onClose={closeAddModal} />}
+                        </div>
+                    ))}
+                </div>
             </div>
-          ))}
         </div>
-        <style jsx>{`
-          .placeholder-top::placeholder {
-            position: relative;
-            top: -130px;
-          }
-        `}</style>
-      </div>
     );
 };
 
