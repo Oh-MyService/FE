762e638342bd74aceb5f2b98ea70af7764090f5b
Osw0 <5196ksy@gmail.com>
2024-07-26 19:14:43 +0900

    로그인 완료 후 이동 삭제

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -20,7 +20,6 @@ const Header = ({ token, setToken }) => {
   const handleLogout = useCallback(() => {
     localStorage.removeItem("token");
     setToken("");
-    goToLogin(); // 로그아웃 후 로그인 페이지로 이동
   }, [setToken, goToLogin]);
 
   return (
