ba5641729e4be5e363e785d3e210bd264f49fe82
Osw0 <5196ksy@gmail.com>
2024-07-27 11:29:34 +0900

    최근생성페이지 이미지 없을 경우

--- src/pages/RecentGeneration.jsx
+++ src/pages/RecentGeneration.jsx
@@ -97,99 +97,113 @@ const RecentGeneration = () => {
             </h1>
           </div>
         </div>
-        <div
-          id="imageDisplayArea"
-          className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-6 gap-4 mt-8"
-        >
-          {items.map((item, index) => {
-            const formattedDate = item.created_at.split("T")[0]; // 날짜 부분만 추출
-            return (
-              <div
-                key={index}
-                className="flex flex-col items-center cursor-pointer relative aspect-square w-full"
-                onClick={() =>
-                  showFullScreenImage(
-                    "data:image/jpeg;base64," + item.image_data
-                  )
-                }
-              >
-                <img
-                  src={"data:image/jpeg;base64," + item.image_data}
-                  alt={"Image ID: " + item.id}
-                  className="w-full h-full object-cover"
-                />
-                <div className="flex justify-between items-center w-full mt-2 font-['pretendard-medium'] text-gray-600">
-                  <p className="text-left">{formattedDate}</p>
-                  <div className="flex items-center space-x-2">
-                    <button
-                      onClick={(e) => {
-                        e.stopPropagation();
-                        openAddModal();
-                      }}
-                    >
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        fill="none"
-                        viewBox="0 0 24 24"
-                        strokeWidth="2"
-                        stroke="currentColor"
-                        className="w-6 h-6"
+        {items.length === 0 ? (
+          <div className="flex flex-col items-center justify-center">
+            <p className="text-center font-['pretendard-extrabold'] text-4xl mb-4 text-black">
+              생성된 패턴이 없습니다. 지금 만들러 가보세요!
+            </p>
+            <button
+              onClick={() => navigate("/create-image")}
+              className="px-6 py-2 border bg-[#3A57A7] hover:bg-[#213261] text-white rounded-full"
+            >
+              패턴 생성하기
+            </button>
+          </div>
+        ) : (
+          <div
+            id="imageDisplayArea"
+            className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-6 gap-4 mt-8"
+          >
+            {items.map((item, index) => {
+              const formattedDate = item.created_at.split("T")[0]; // 날짜 부분만 추출
+              return (
+                <div
+                  key={index}
+                  className="flex flex-col items-center cursor-pointer relative aspect-square w-full"
+                  onClick={() =>
+                    showFullScreenImage(
+                      "data:image/jpeg;base64," + item.image_data
+                    )
+                  }
+                >
+                  <img
+                    src={"data:image/jpeg;base64," + item.image_data}
+                    alt={"Image ID: " + item.id}
+                    className="w-full h-full object-cover"
+                  />
+                  <div className="flex justify-between items-center w-full mt-2 font-['pretendard-medium'] text-gray-600">
+                    <p className="text-left">{formattedDate}</p>
+                    <div className="flex items-center space-x-2">
+                      <button
+                        onClick={(e) => {
+                          e.stopPropagation();
+                          openAddModal();
+                        }}
                       >
-                        <path
-                          strokeLinecap="round"
-                          strokeLinejoin="round"
-                          d="M17.593 3.322c1.1.128 1.907 1.077 1.907 2.185V21L12 17.25 4.5 21V5.507c0-1.108.806-2.057 1.907-2.185a48.507 48.507 0 0 1 11.186 0Z"
-                        />
-                      </svg>
-                    </button>
-                    <button
-                      onClick={(e) => {
-                        e.stopPropagation();
-                        openDeleteModal(index);
-                      }}
-                    >
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        fill="none"
-                        viewBox="0 0 24 24"
-                        strokeWidth="2"
-                        stroke="currentColor"
-                        className="w-6 h-6"
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          fill="none"
+                          viewBox="0 0 24 24"
+                          strokeWidth="2"
+                          stroke="currentColor"
+                          className="w-6 h-6"
+                        >
+                          <path
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            d="M17.593 3.322c1.1.128 1.907 1.077 1.907 2.185V21L12 17.25 4.5 21V5.507c0-1.108.806-2.057 1.907-2.185a48.507 48.507 0 0 1 11.186 0Z"
+                          />
+                        </svg>
+                      </button>
+                      <button
+                        onClick={(e) => {
+                          e.stopPropagation();
+                          openDeleteModal(index);
+                        }}
                       >
-                        <path
-                          strokeLinecap="round"
-                          strokeLinejoin="round"
-                          d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0 a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0"
-                        />
-                      </svg>
-                    </button>
-                    <button
-                      onClick={(e) => {
-                        e.stopPropagation();
-                        handleSaveImage();
-                      }}
-                    >
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        fill="none"
-                        viewBox="0 0 24 24"
-                        strokeWidth="2"
-                        stroke="currentColor"
-                        className="w-6 h-6"
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          fill="none"
+                          viewBox="0 0 24 24"
+                          strokeWidth="2"
+                          stroke="currentColor"
+                          className="w-6 h-6"
+                        >
+                          <path
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0 a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0"
+                          />
+                        </svg>
+                      </button>
+                      <button
+                        onClick={(e) => {
+                          e.stopPropagation();
+                          handleSaveImage();
+                        }}
                       >
-                        <path
-                          strokeLinecap="round"
-                          strokeLinejoin="round"
-                          d="M3 16.5v2.25A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75V16.5M16.5 12 12 16.5m0 0L7.5 12m4.5 4.5V3"
-                        />
-                      </svg>
-                    </button>
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          fill="none"
+                          viewBox="0 0 24 24"
+                          strokeWidth="2"
+                          stroke="currentColor"
+                          className="w-6 h-6"
+                        >
+                          <path
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            d="M3 16.5v2.25A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75V16.5M16.5 12 12 16.5m0 0L7.5 12m4.5 4.5V3"
+                          />
+                        </svg>
+                      </button>
+                    </div>
                   </div>
                 </div>
-              </div>
-            );
-          })}
-        </div>
+              );
+            })}
+          </div>
+        )}
         {fullScreenImage && (
           <div
             className="fixed inset-0 bg-black bg-opacity-85 flex items-center justify-center p-4"
