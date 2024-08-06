00e117178b68ef5b9ba2bc4750e357ccb6557dc6
Osw0 <5196ksy@gmail.com>
2024-08-06 15:38:50 +0900

    마이페이지 로딩 추가

--- src/pages/RecentGeneration.jsx
+++ src/pages/RecentGeneration.jsx
@@ -145,7 +145,7 @@ const RecentGeneration = () => {
             <div role="status">
               <svg
                 aria-hidden="true"
-                className="w-20 h-20 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600"
+                className="w-20 h-20 text-gray-200 animate-spin fill-blue-600"
                 viewBox="0 0 100 101"
                 fill="none"
                 xmlns="http://www.w3.org/2000/svg"
@@ -159,7 +159,6 @@ const RecentGeneration = () => {
                   fill="currentFill"
                 />
               </svg>
-              <span className="sr-only">Loading...</span>
             </div>
           </div>
         ) : items.length === 0 ? (
