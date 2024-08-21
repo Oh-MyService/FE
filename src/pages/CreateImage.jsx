cedcdfb7028981c12da4424fa0ed783cf3492291
zw0n2 <daniela0412@naver.com>
2024-08-21 17:49:09 +0900

    사진 이동

--- src/pages/CreateImage.jsx
+++ src/pages/CreateImage.jsx
@@ -517,7 +517,7 @@ const CreateImage = () => {
                             </div>
                             <div className="grid grid-cols-2 gap-4 mt-8">
                                 {result.images.map((imageData, idx) => (
-                                    <div key={idx} className="flex flex-col justify-between items-center w-40 ml-4">
+                                    <div key={idx} className="flex flex-col justify-between items-center w-40 ml-10">
                                         <div className="overflow-hidden w-60 h-60 cursor-pointer">
                                             <img
                                                 src={`data:image/jpeg;base64,${imageData}`}
