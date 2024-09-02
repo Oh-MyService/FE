78b3e401ef4f0de17791cda56093f5ac01fb7086
zw0n2 <daniela0412@naver.com>
2024-09-02 16:52:43 +0900

    두께 변경

--- src/pages/RecentGeneration.jsx
+++ src/pages/RecentGeneration.jsx
@@ -358,7 +358,7 @@ const RecentGeneration = () => {
                         xmlns="http://www.w3.org/2000/svg"
                         fill="none"
                         viewBox="0 0 24 24"
-                        stroke-width="2"
+                        stroke-width="3"
                         stroke="currentColor"
                         className="size-6 text-white"
                     >
