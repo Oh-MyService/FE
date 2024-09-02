3ac8be146c1da3b8ab2874ba1bdc4c54715b1bed
zw0n2 <daniela0412@naver.com>
2024-09-02 16:44:32 +0900

    석환짱

--- src/pages/RecentGeneration.jsx
+++ src/pages/RecentGeneration.jsx
@@ -358,9 +358,9 @@ const RecentGeneration = () => {
                         xmlns="http://www.w3.org/2000/svg"
                         fill="none"
                         viewBox="0 0 24 24"
-                        stroke-width="1.5"
+                        stroke-width="2"
                         stroke="currentColor"
-                        class="size-6"
+                        className="size-6 text-white"
                     >
                         <path stroke-linecap="round" stroke-linejoin="round" d="M4.5 10.5 12 3m0 0 7.5 7.5M12 3v18" />
                     </svg>
