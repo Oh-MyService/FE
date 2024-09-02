9d19db744af57a884f92117d5311ba6ef5e9d46d
zw0n2 <daniela0412@naver.com>
2024-09-02 16:38:31 +0900

    맨위로 디자인 변경

--- src/pages/RecentGeneration.jsx
+++ src/pages/RecentGeneration.jsx
@@ -354,12 +354,15 @@ const RecentGeneration = () => {
                     onClick={scrollToTop}
                     className="fixed bottom-8 right-8 bg-[#3A57A7] p-3 rounded-full shadow-lg hover:bg-[#213261] transition"
                 >
-                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="white" className="w-6 h-6">
-                        <path
-                            fillRule="evenodd"
-                            d="M12 2.25c-5.385 0-9.75 4.365-9.75 9.75s4.365 9.75 9.75 9.75 9.75-4.365 9.75-9.75S17.385 2.25 12 2.25Zm.53 5.47a.75.75 0 0 0-1.06 0l-3 3a.75.75 0 1 0 1.06 1.06l1.72-1.72v5.69a.75.75 0 0 0 1.5 0v-5.69l1.72 1.72a.75.75 0 1 0 1.06-1.06l-3-3Z"
-                            clipRule="evenodd"
-                        />
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        stroke-width="1.5"
+                        stroke="currentColor"
+                        class="size-6"
+                    >
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M4.5 10.5 12 3m0 0 7.5 7.5M12 3v18" />
                     </svg>
                 </button>
             )}
