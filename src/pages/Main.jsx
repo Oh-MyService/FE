125382085508ef3916693b173a21e2a4fe448dcb
zw0n2 <daniela0412@naver.com>
2024-09-29 11:39:33 +0900

    반응형 수정

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -9,12 +9,30 @@ function Main() {
         >
             <style>
                 {`
+        @media (max-width: 1024px) {
+          .text-6xl {
+            font-size: 3rem;
+          }
+          .text-2xl {
+            font-size: 1.5rem;
+          }
+          .pl-80 {
+            padding-left: 40px;
+          }
+        }
+
         @media (max-width: 768px) {
           .text-6xl {
-            font-size: 2.5rem;
+            font-size: 2rem;
           }
           .text-2xl {
             font-size: 1rem;
+          }
+          .pl-80 {
+            padding-left: 20px;
+          }
+          img {
+            width: 90%;
           }
         }
         `}
@@ -24,13 +42,12 @@ function Main() {
             </h1>
             <p
                 className="text-2xl text-left font-['pretendard-medium'] pl-80 pt-10 z-10"
-                style={{ lineHeight: '1.5em' }}
+                style={{ lineHeight: '1.4em' }}
             >
                 원하는 패턴이 없으신가요? <br /> AI를 사용해 직접 만들어보세요!
             </p>
 
-            {/* 이미지 삽입 */}
-            <div className="flex justify-center items-center w-full" style={{ marginTop: '60px' }}>
+            <div className="flex justify-center items-center w-full" style={{ marginTop: '40px' }}>
                 <img src={TipImage} alt="Tip Image" className="w-3/4 h-auto" style={{ borderRadius: '26px' }} />
             </div>
         </div>
