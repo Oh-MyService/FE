f5de1023e501fe6238ececf9b86a0b817915abc0
Osw0 <5196ksy@gmail.com>
2024-09-29 14:47:19 +0900

    크기 수정

--- src/pages/RecentGeneration.jsx
+++ src/pages/RecentGeneration.jsx
@@ -341,7 +341,7 @@ const RecentGeneration = () => {
           >
             <div
               className="grid grid-cols-3 gap-0"
-              style={{ width: "min(45vw, 45vh)", height: "min(45vw, 45vh)" }}
+              style={{ width: "min(80vw, 80vh)", height: "min(80vw, 80vh)" }}
             >
               {Array.from({ length: 9 }).map((_, index) => (
                 <img
