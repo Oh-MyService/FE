537b5fdb52476be95d458173d6e42c40fd9307a3
zw0n2 <daniela0412@naver.com>
2024-10-03 15:23:52 +0900

    메인 디자인 수정

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
-import TipImage from '../assets/tip.png';
+import TipImage from '../assets/image.png';
+import MainBg from '../assets/main_bg.jpg'; // 배경 이미지 가져오기
 
 function Main() {
     return (
         <div
-            className="bg-[#F2F2F2] h-screen w-full bg-cover bg-center bg-no-repeat flex flex-col justify-center items-start p-8 relative overflow-hidden"
+            className="relative h-screen w-full flex flex-col justify-center items-start p-8 overflow-hidden"
             style={{ paddingTop: '100px' }}
         >
             <style>
@@ -36,8 +37,26 @@ function Main() {
             margin-left: 10px;
           }
         }
+
+        /* 배경에만 투명도 40% 적용 */
+        .background::before {
+          content: '';
+          position: absolute;
+          top: 0;
+          left: 0;
+          right: 0;
+          bottom: 0;
+          background-image: url(${MainBg});
+          background-size: cover;
+          background-position: center;
+          opacity: 0.4; /* 배경 이미지에만 투명도 적용 */
+          z-index: -1; /* 배경 이미지가 텍스트 뒤에 있도록 설정 */
+        }
         `}
             </style>
+
+            <div className="background absolute top-0 left-0 w-full h-full"></div>
+
             <h1 className="text-6xl font-['pretendard-black'] text-left pl-80 z-10">
                 세상의 모든 원단 디자인, 디자이노블에서
             </h1>
