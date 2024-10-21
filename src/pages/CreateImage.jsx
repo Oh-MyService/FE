495c4edd0c05afefe1b31d2967ac739a0dc06817
zw0n2 <daniela0412@naver.com>
2024-10-21 14:22:15 +0900

    새고해도 진행

--- src/pages/CreateImage.jsx
+++ src/pages/CreateImage.jsx
@@ -218,14 +218,18 @@ const CreateImage = () => {
     useEffect(() => {
         const savedResults = localStorage.getItem('results');
         if (savedResults) {
-            setResults(
-                JSON.parse(savedResults).map((result) => ({
-                    ...result,
-                    isLoading: result.images && result.images.length > 0 ? false : true,
-                }))
-            );
+            const parsedResults = JSON.parse(savedResults);
+
+            setResults(parsedResults);
+
+            // isLoading 상태인 경우 프로그래스 폴링 시작
+            parsedResults.forEach((result) => {
+                if (result.isLoading) {
+                    fetchProgress(result.task_id); // task_id별로 진행 상황 확인
+                }
+            });
         }
-    }, []);
+    }, []); // 컴포넌트가 마운트될 때 한번만 실행
 
     // results가 변경될 때마다 로컬 스토리지에 기록 저장
     useEffect(() => {
@@ -729,7 +733,7 @@ const CreateImage = () => {
                                     <div className="flex items-center mt-4">
                                         <div className="flex-grow h-2.5 bg-gray-300 rounded-full overflow-hidden">
                                             <div
-                                                className="h-full bg-[#809DEC]"
+                                                className="h-full bg-[#444655]"
                                                 style={{ width: `${result.progress}%` }}
                                             ></div>
                                         </div>
