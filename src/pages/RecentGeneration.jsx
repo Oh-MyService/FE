d5ea2f869bb97c674f14e483441795d4588acc0a
zw0n2 <daniela0412@naver.com>
2024-10-26 14:41:21 +0900

    풀스크린 이미지 크기 수정

--- src/pages/RecentGeneration.jsx
+++ src/pages/RecentGeneration.jsx
@@ -360,9 +360,9 @@ const RecentGeneration = () => {
                         className="fixed inset-0 bg-black bg-opacity-85 flex items-center justify-center z-[1000]"
                         onClick={closeFullScreen}
                     >
-                        <div className="flex space-x-4" style={{ maxWidth: '80vw', maxHeight: '80vh' }}>
+                        <div className="flex space-x-4" style={{ maxWidth: '75vw', maxHeight: '75vh' }}>
                             {/* 원본 이미지 */}
-                            <div className="flex flex-col items-start" style={{ width: '30%', height: '100%' }}>
+                            <div className="flex flex-col items-start" style={{ width: '25%', height: '100%' }}>
                                 <p className="text-white bg-black bg-opacity-50 px-2 py-1 rounded mb-2">
                                     Original Image
                                 </p>
@@ -375,7 +375,7 @@ const RecentGeneration = () => {
                                 </div>
                             </div>
                             {/* 3x3 타일링된 이미지 */}
-                            <div className="flex flex-col items-start" style={{ width: '60%', height: '100%' }}>
+                            <div className="flex flex-col items-start" style={{ width: '50%', height: '100%' }}>
                                 <p className="text-white bg-black bg-opacity-50 px-2 py-1 rounded mb-2">
                                     3×3 Tiled Pattern
                                 </p>
